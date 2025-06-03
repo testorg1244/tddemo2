7441a21d1f74803d32139c10baf05439f43c5efa
Markus Lippert <markuslippert@outlook.com>
2025-06-03 15:37:33 +0200

    Initial commit

---
+++ .alpaca/PowerShell/Remove-AlpacaContainer.ps1
@@ -0,0 +1,26 @@
+param (
+    [string]$token
+)
+
+Import-Module ".\.alpaca\PowerShell\module\alpaca-functions.psd1" -Scope Global -Force -DisableNameChecking
+
+$Needs = $ENV:needsContext | ConvertFrom-Json
+$containerConfig = $Needs."CustomJob-CreateAlpaca-Container".outputs
+
+$owner = $Env:GITHUB_REPOSITORY_OWNER
+$repository = $Env:GITHUB_REPOSITORY
+$repository = $repository.replace($owner, "")
+$repository = $repository.replace("/", "")
+$branch = $Env:GITHUB_REF_NAME
+
+$containerID = $containerConfig.containerID
+
+Write-Host "Delete Container $containerID"
+
+$headers = Get-AuthenticationHeader -token $token -owner $owner -repository $repository
+
+$QueryParams = @{
+    "api-version" = "0.12"
+}
+$apiUrl = Get-AlpacaEndpointUrlWithParam -controller "Container" -ressource $containerID -QueryParams $QueryParams
+Invoke-RestMethod $apiUrl -Method 'DELETE' -Headers $headers -AllowInsecureRedirect
